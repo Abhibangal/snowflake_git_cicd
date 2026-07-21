c845b910fcf5acfef447b9956b60268a92b3c714
Abhijit Bangal <u514526@wns.com>
2026-07-21 19:22:00 +0530

    added new object for demo

--- snowflake/grants/RAW/CUSTOMER_HUB/R__grant_emp_dept_sp.sql
+++
@@ -1,8 +0,0 @@
--- Grants for EMP_DEPT_SP in RAW / CUSTOMER_HUB.
--- grant_role resolves from folder path + branch environment:
---   snowflake/grants/RAW/CUSTOMER_HUB/... -> AR_DEV_RAW_CUSTOMERHUB_RW on dev
---   same path on main                         -> AR_PROD_RAW_CUSTOMERHUB_RW
-
-GRANT OWNERSHIP ON PROCEDURE EMP_DEPT_SP()
-    TO ROLE {{ grant_role }}
-    COPY CURRENT GRANTS;
